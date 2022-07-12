8be9cc6828d67d4f820d83e3b3b00dcc369b3664
Cosmo <carlosjrlu@gmail.com>
2022-07-11 21:31:58 -0500

    Initial

---
+++ src/main/java/net/cosmocorban/polystorage/PolyStorageClient.java
@@ -0,0 +1,10 @@
+package net.cosmocorban.polystorage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PolyStorageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
